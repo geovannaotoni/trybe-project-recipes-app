7ed4cc6574b46782b0b1d1c3b1045f5f99f763d9
Geovanna Otoni <geovannaotoni@gmail.com>
2023-06-05 16:15:07 -0300

    fix: cria pasta de Login dentro do diretorio pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './pages/Login';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
