bbf448b87d730e641db731866cb003d488f4b890
Geovanna Otoni <geovannaotoni@gmail.com>
2023-06-05 16:25:32 -0300

    feat02: testes para a page login

--- src/tests/App.test.js
+++
@@ -1,8 +0,0 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import App from '../App';
-
-test('Farewell, front-end', () => {
-  // Este arquivo pode ser modificado ou deletado sem problemas
-  // render(<App />);
-});
