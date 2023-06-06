e2b8e2360af157f78168a8dab2f6ca9bdf7b1fed
Geovanna Otoni <geovannaotoni@gmail.com>
2023-06-06 09:12:06 -0300

    feat: testes para o componente header

---
+++ src/tests/mocks/Meals.js
@@ -0,0 +1,66 @@
+const mealsData = {
+  meals: [
+    {
+      strMeal: 'Apam balik',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/adxcbq1619787919.jpg',
+      idMeal: '53049',
+    },
+    {
+      strMeal: 'Apple & Blackberry Crumble',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/xvsurr1511719182.jpg',
+      idMeal: '52893',
+    },
+    {
+      strMeal: 'Apple Frangipan Tart',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/wxywrq1468235067.jpg',
+      idMeal: '52768',
+    },
+    {
+      strMeal: 'Baingan Bharta',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/urtpqw1487341253.jpg',
+      idMeal: '52807',
+    },
+    {
+      strMeal: 'Baked salmon with fennel & tomatoes',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/1548772327.jpg',
+      idMeal: '52959',
+    },
+    {
+      strMeal: 'Bakewell tart',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/wyrqqq1468233628.jpg',
+      idMeal: '52767',
+    },
+    {
+      strMeal: 'Banana Pancakes',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/sywswr1511383814.jpg',
+      idMeal: '52855',
+    },
+    {
+      strMeal: 'Bean & Sausage Hotpot',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/vxuyrx1511302687.jpg',
+      idMeal: '52848',
+    },
+    {
+      strMeal: 'Beef Banh Mi Bowls with Sriracha Mayo, Carrot & Pickled Cucumber',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/z0ageb1583189517.jpg',
+      idMeal: '52997',
+    },
+    {
+      strMeal: 'Bistek',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/4pqimk1683207418.jpg',
+      idMeal: '53069',
+    },
+    {
+      strMeal: 'Boulangère Potatoes',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/qywups1511796761.jpg',
+      idMeal: '52914',
+    },
+    {
+      strMeal: 'Bread and Butter Pudding',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/xqwwpy1483908697.jpg',
+      idMeal: '52792',
+    },
+  ],
+};
+
+export default mealsData;
