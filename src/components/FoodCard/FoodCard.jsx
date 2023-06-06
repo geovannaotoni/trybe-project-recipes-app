4e3637fae24d50dc432842021c92acbe4b785379
GelFerreira <gelvana_princesaanahi@hotmail.com>
2023-06-06 17:00:34 -0300

    feat04: toggle filter + redirect to details

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -7,15 +7,28 @@ export default function FoodCard(props) {
   // History
   const history = useHistory();
   const { location: { pathname } } = history;
-  // const path = API_URL.toParam(pathname);
+  const path = API_URL.toParam(pathname);
   // const pathCap = API_URL.toCapitalize(pathname);
   const foodType = API_URL.toSingleParam(pathname);
   // console.log(props); --> results e index
   const { index, result } = props;
+
+  const redirectToDetail = () => {
+    const id = result[`id${foodType}`];
+    const newPath = `/${path}/${id}`;
+    history.push(newPath);
+  };
+
+  // Retorno visual
   return (
     <div
       className="FoodCard"
       data-testid={ `${index}-recipe-card` }
+      style={ {
+        border: '1px solid black',
+      } }
+      onClick={ redirectToDetail }
+      aria-hidden
     >
       <img
         style={ { width: '100px' } }
