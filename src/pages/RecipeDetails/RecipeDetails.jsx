65a2b4a721a521f4471f11a4ba0b6b6550d501cc
henrique-marcarini <henrique_dutra_10@hotmail.com>
2023-06-06 17:32:16 -0300

    feat: iniciando o requisito 25

--- src/pages/RecipeDetails/RecipeDetails.jsx
+++ src/pages/RecipeDetails/RecipeDetails.jsx
@@ -4,8 +4,10 @@ import { fetchAPI } from '../../services/fetchAPI';
 
 function RecipeDetails() {
   const history = useHistory();
-
-  const [element, setElement] = useState();
+  const currentPath = history.location.pathname;
+  const parts = currentPath.split('/');
+  const [, type, id] = parts;
+  const [element, setElement] = useState(null);
   // const [user setUser] = useState({
   //   name:'',
   //   lastName: '',
@@ -14,26 +16,51 @@ function RecipeDetails() {
 
   // user.adress
   // setUser(user.lastName = 'Silva')
-  const returnRote = async (type, id) => {
-    if (type === 'meals') {
-      const meals = await fetchAPI(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+
+  const getIngredients = (array) => {
+    const ingredients = Object.keys(array).filter((k) => k.includes('strIngredient'));
+    console.log(ingredients);
+  };
+
+  const returnRote = async (tipo, ide) => {
+    if (tipo === 'meals') {
+      const meals = await fetchAPI(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${ide}`);
       setElement(await meals.meals[0]);
-    } else if (type === 'drinks') {
+    } else if (tipo === 'drinks') {
       // http://localhost:3000/drinks/14053
-      const drinks = await fetchAPI(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
-      setElement(await drinks.drinks[0]);
+      const drinks = await fetchAPI(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${ide}`);
+      setElement(drinks.drinks[0]);
+      // console.log(drinks);
     }
   };
 
   useEffect(() => {
-    const currentPath = history.location.pathname;
-    const parts = currentPath.split('/');
-    const [, type, id] = parts;
     returnRote(type, id);
-  }, []);
-
+  }, [id, type]);
+  // console.log(element);
+  getIngredients(element);
   return (
-    <div>{element}</div>
+    <div>
+      <h1>aaa</h1>
+      <h3>asd</h3>
+      {
+        element && (
+          <div>
+            <p data-testid="recipe-title">{element.strDrink}</p>
+            <img
+              src={ element.strDrinkThumb }
+              alt={ element.strDrink }
+              data-testid="recipe-photo"
+            />
+            <p data-testid="recipe-category">{element.strCategory}</p>
+            <p data-testid={ `${element.index}-ingredient-name-and-measure` }>
+              {element.strIngredient1}
+            </p>
+            <p data-testid="instructions">{element.strInstructions}</p>
+          </div>
+        )
+      }
+    </div>
   );
 }
 
