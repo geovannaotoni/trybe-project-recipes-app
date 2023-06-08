a2f5479f86e5f5f45361dab0afc45612f2edb301
henrique-marcarini <henrique_dutra_10@hotmail.com>
2023-06-08 12:37:51 -0300

    fix: voltando mudancas no RecipeDetails.test

--- src/tests/RecipeDetails.test.js
+++ src/tests/RecipeDetails.test.js
@@ -1,49 +1,60 @@
 import React from 'react';
-import { screen, act } from '@testing-library/react';
+import { screen, act, waitFor } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from './utils/renderWithRouter';
 
 describe('Testes para a página de RecipeDetails', () => {
-  test('Se está renderizando corretamente a pagina de meals', () => {
+  test('Se está renderizando corretamente a pagina de meals', async () => {
     const { history } = renderWithRouter(<App />);
     act(() => {
       history.push('/meals/52771');
     });
     expect(history.location.pathname).toBe('/meals/52771');
-
-    const recipeTitle = screen.getByTestId('recipe-title');
-    expect(recipeTitle.textContent).toBe('Spicy Arrabiata Penne');
+    await waitFor(() => {
+      const recipeTitle = screen.getByTestId('recipe-title');
+      expect(recipeTitle.textContent).toBe('Spicy Arrabiata Penne');
+    });
+    await waitFor(() => {
+      const recipePhoto = screen.getByTestId('recipe-photo');
+      expect(recipePhoto.getAttribute('alt')).toBe('Spicy Arrabiata Penne');
+    });
+    await waitFor(() => {
+      const recipeCategory = screen.getByTestId('recipe-category');
+      expect(recipeCategory.textContent).toBe('Vegetarian');
+    });
+    await waitFor(() => {
+      const ingredient = screen.getByTestId('0-ingredient-name-and-measure');
+      expect(ingredient.textContent).toBe('penne rigate - 1 pound');
+    });
+    await waitFor(() => {
+      const video = screen.queryByTestId('video');
+      expect(video).toBeInTheDocument();
+    });
   });
-
-  const recipePhoto = screen.getByTestId('recipe-photo');
-  expect(recipePhoto.getAttribute('alt')).toBe('Spicy Arrabiata Penne');
-});
-
-const recipeCategory = screen.getByTestId('recipe-category');
-expect(recipeCategory.textContent).toBe('Vegetarian');
-
-const ingredient = screen.getByTestId('0-ingredient-name-and-measure');
-expect(ingredient.textContent).toBe('penne rigate - 1 pound');
-
-const video = screen.queryByTestId('video');
-expect(video).toBeInTheDocument();
-test('Se está renderizando corretamente a pagina de drinks', async () => {
-  const { history } = renderWithRouter(<App />);
-  act(() => {
-    history.push('/drinks/14053');
+  test('Se está renderizando corretamente a pagina de drinks', async () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/drinks/14053');
+    });
+    const title = 'Mother\'s Milk';
+    expect(history.location.pathname).toBe('/drinks/14053');
+    const videoElement = screen.queryByTestId('video');
+    expect(videoElement).toBeNull();
+    await waitFor(() => {
+      const recipeTitle = screen.getByTestId('recipe-title');
+      expect(recipeTitle.textContent).toBe(title);
+    });
+    await waitFor(() => {
+      const recipePhoto = screen.getByTestId('recipe-photo');
+      expect(recipePhoto.getAttribute('alt')).toBe(title);
+    });
+    await waitFor(() => {
+      const recipeCategory = screen.getByTestId('recipe-category');
+      expect(recipeCategory.textContent).toBe('Alcoholic');
+    });
+    await waitFor(() => {
+      const ingredient = screen.getByTestId('0-ingredient-name-and-measure');
+      expect(ingredient.textContent).toBe('Goldschlager - 1 oz');
+    });
   });
-  const title = 'Mother\'s Milk';
-  expect(history.location.pathname).toBe('/drinks/14053');
-  const videoElement = screen.queryByTestId('video');
-  expect(videoElement).toBeNull();
-
-  const recipeTitle = screen.getByTestId('recipe-title');
-  expect(recipeTitle.textContent).toBe(title);
-
-  const recipePhoto = screen.getByTestId('recipe-photo');
-  expect(recipePhoto.getAttribute('alt')).toBe(title);
-
-  expect(recipeCategory.textContent).toBe('Alcoholic');
-
-  expect(ingredient.textContent).toBe('Goldschlager - 1 oz');
 });
