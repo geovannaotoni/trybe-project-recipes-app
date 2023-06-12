f6c73cfd9e5cba6cb0ec15e41fbd37d0e7c76363
Amanayara Slot <amanayaradev@gmail.com>
2023-06-12 15:25:09 -0300

    feat: favoriteRecipes

--- src/pages/FavoriteRecipes/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes/FavoriteRecipes.jsx
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header/Header';
 import useFilterButtons from '../../hooks/useFilterButtons/useFilterButtons';
+import useRecipeCards from '../../hooks/useRecipeCards/useRecipeCards';
+import favoriteBlackIcon from '../../images/blackHeartIcon.svg';
+import { getFromStorage, setOnStorage } from '../../services/localStorage';
 
 function FavoriteRecipes() {
   const { filterType, renderButtons } = useFilterButtons();
+  const { shareBtn, renderRecipeCard } = useRecipeCards();
+  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+
+  useEffect(() => {
+    const favoriteRecipesFromStorage = getFromStorage('favoriteRecipes');
+    if (favoriteRecipesFromStorage) {
+      setFavoriteRecipes(favoriteRecipesFromStorage);
+    }
+  }, []);
+  const handleFavorite = (recipe) => {
+    const newFavoriteList = favoriteRecipes.filter((favRecipe) => (
+      favRecipe.id !== recipe.id
+    ));
+    setOnStorage('favoriteRecipes', newFavoriteList);
+    setFavoriteRecipes(newFavoriteList);
+  };
 
   return (
     <div>
       <Header pageTitle="Favorite Recipes" />
       { renderButtons() }
       <section>
-        {filterType}
+        {
+          favoriteRecipes
+            .filter((recipe) => recipe.type.includes(filterType))
+            .map((recipe, index) => (
+              <article key={ index }>
+                {renderRecipeCard(recipe, index)}
+                <button onClick={ () => handleFavorite(recipe) } type="button">
+                  <img
+                    data-testid={ `${index}-horizontal-favorite-btn` }
+                    src={ favoriteBlackIcon }
+                    alt="favoriteIco"
+                  />
+                </button>
+              </article>
+            ))
+        }
+        { shareBtn && <p>Link copied!</p>}
       </section>
     </div>
   );
